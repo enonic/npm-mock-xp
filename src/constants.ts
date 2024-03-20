2e04142858e28fac18a802c631299b9ecaf362fc
Christian Westgaard <comlock@gmail.com>
2024-03-20 16:46:46 +0100

    cleanup

--- src/constants.ts
+++
@@ -1,4 +0,0 @@
-export const CONTENT_TYPE_BASE_FOLDER = 'base:folder';
-export const CONTENT_TYPE_PORTAL_SITE = 'portal:site';
-
-export const SYSTEM_REPO = 'system-repo';
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-export {JavaBridge} from './JavaBridge'
+export {App} from './implementation/App';
+export {Branch} from './implementation/Branch';
+export {ContentConnection} from './implementation/ContentConnection';
+export {Log} from './implementation/Log';
+export {Project} from './implementation/Project';
+export {Repo} from './implementation/Repo';
+export {RepoConnection} from './implementation/RepoConnection';
+export {Request} from './implementation/Request';
+export {Server} from './implementation/Server';
+export {Version} from './implementation/Version';
+
+export {LibContent} from './libs/LibContent';
+export {LibPortal} from './libs/LibPortal';
