aaef3caec9e82f62e57f3306576f2615f365946b
Christian Westgaard <comlock@gmail.com>
2024-04-15 08:58:06 +0200

    Huh?

--- test/constants.ts
+++ test/constants.ts
@@ -1,4 +1,4 @@
 export const BUN = typeof Bun !== "undefined";
 
 export const LEA_JPG_BYTE_SIZE = BUN ? 526283 : 528238;
-export const THUR_BYTE_SIZE = BUN ? 186329 : 187348;
+export const THUR_BYTE_SIZE = BUN ? 186328 : 187348;
