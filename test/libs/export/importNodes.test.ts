ee151634f539e5c8cace0583bffc90dca5801456
Christian Westgaard <christian.westgaard@helsedir.no>
2025-10-06 14:32:05 +0200

    Minimal export zip

--- test/libs/export/importNodes.test.ts
+++ test/libs/export/importNodes.test.ts
@@ -9,10 +9,12 @@ import { resolve } from 'path';
 
 // const SANDBOX_NAME = 'mysandbox';
 const SOURCE = 'myexport';
-const ABS_PATH_SOURCE_ZIP = resolve(__dirname,'export.zip');
+const ABS_PATH_SOURCE_ZIP = resolve(
+	__dirname,
+	'com.enonic.cms.my-project-draft-2025-10-06T14-27-50.zip'
+);
 const PROJECT_NAME = 'myproject';
 const REPO_ID = `com.enonic.cms.${PROJECT_NAME}`;
-const ADDED_COUNT = 98;
 
 let
 	connection: RepoConnection,
@@ -71,7 +73,7 @@ describe('LibExport', () => {
 				// nodeImported: () => {},
 				// nodeResolved: () => {},
 			});
-			expect(ImportNodesResult.addedNodes.length).toBe(ADDED_COUNT);
+			expect(ImportNodesResult.addedNodes.length).toBe(8);
 			expect(ImportNodesResult.updatedNodes).toStrictEqual([
 				'00000000-0000-0000-0000-000000000000'
 			]);
@@ -95,7 +97,17 @@ describe('LibExport', () => {
 				// nodeImported: () => {},
 				// nodeResolved: () => {},
 			});
-			expect(ImportNodesResult.addedNodes.length).toBe(ADDED_COUNT);
+			expect(ImportNodesResult.addedNodes.length).toBe(8);
+			expect(ImportNodesResult.addedNodes).toStrictEqual([
+				'2f9d32c6-387f-4d3e-aceb-5976b207cc4b',
+				'7212bb0d-6a0f-48a1-a85f-cc856817aebe',
+				'ba727b82-813e-416e-9d11-30a9c60399d9',
+				'9f6c6353-da7b-42da-9300-64be10f7ba0c',
+				'4f62c049-4995-4db7-87e4-364d582943ef',
+				'acc48303-8144-43b1-b69f-d5dcfe935b67',
+				'c61f74a2-7cc4-4160-b037-ed3f581dfb71',
+				'45cc4b1a-343c-4636-ae8f-15cd977d3b77'
+			]);
 			expect(ImportNodesResult.updatedNodes).toStrictEqual([
 				'00000000-0000-0000-0000-000000000000'
 			]);
